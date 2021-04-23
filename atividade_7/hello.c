80a5a804f4c492e376958b24df9f2f9c55256538
Maria Luísa Lima <mlll@cin.ufpe.br>
2021-04-23 14:36:42 -0300

    done

--- atividade_7/hello.c
+++ atividade_7/hello.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/moduleparam.h>
 MODULE_LICENSE("Dual BSD/GPL");
+static int a = 9999;
+module_param(a, int, 0);
 
 static int __init hello_init(void)
 {
-    printk(KERN_LOG "Hello, World\n");
+    printk(KERN_INFO "Hello, new Worlds\n");
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    printk(KERN_LOG "Bye, World\n");
+    printk(KERN_INFO "Bye, %d Worlds\n", a);
 }
 
 module_init(hello_init);
